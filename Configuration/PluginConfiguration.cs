71fd0f11e1cfbdb1f796d63d1d82cf49927a0583
James Lounds <jameslounds@gmail.com>
2024-12-14 13:45:19 +0000

    Initial commit

---
+++ Configuration/PluginConfiguration.cs
@@ -0,0 +1,17 @@
+using MediaBrowser.Model.Plugins;
+
+namespace JellyfinPluginMediaSegmentEditorEndpoints.Configuration;
+
+/// <summary>
+/// The configuration options.
+/// </summary>
+public class PluginConfiguration : BasePluginConfiguration
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="PluginConfiguration"/> class.
+    /// </summary>
+    public PluginConfiguration()
+    {
+        // pass
+    }    
+}
